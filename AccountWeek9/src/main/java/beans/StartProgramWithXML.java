7b9f71b7250e317da0001d5e8ea1a9df4d496744
Danh Tran <dtran6@dmacc.edu>
2017-12-04 21:48:53 -0600

    Correct getAccount(2)

--- AccountWeek9/src/main/java/beans/StartProgramWithXML.java
+++ AccountWeek9/src/main/java/beans/StartProgramWithXML.java
@@ -13,12 +13,12 @@ public class StartProgramWithXML {
 		
 		System.out.println("Before money transfer");
 		System.out.println("Account 1 Balance: " + accountService.getAccount(1).getBalance());
-		System.out.println("Account 2 Balance: " + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 Balance: " + accountService.getAccount(2).getBalance());
 		
 		accountService.transferMoney(1, 2, 5);
 		
 		System.out.println("After money transfer");
 		System.out.println("Account 1 Balance: " + accountService.getAccount(1).getBalance());
-		System.out.println("Account 2 Balance: " + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 Balance: " + accountService.getAccount(2).getBalance());
 	}
 }
